66d2501d7a8cabb6d2ed38ad9da627670214d993
Nayeem Rahman <nayeem12rahman@gmail.com>
2022-10-05 01:29:48 +0600

    Composition #6

---
+++ OOD/Composition/DbMigrator.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Composition;
+
+public class DbMigrator
+{
+	private readonly Logger _logger;
+
+	public DbMigrator(Logger logger)
+	{
+		_logger = logger;
+	}
+
+	public void Migrate()
+	{
+		_logger.Log("Migrating...");
+	}
+}
