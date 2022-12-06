6d128624648fdf7fb8032f250d45d3dc61d2339e
Nayeem Rahman <nayeem12rahman@gmail.com>
2022-12-06 08:40:02 +0600

    DIP V2 added

---
+++ OOD/SOLID/DIP_V2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    public class DIP_V2
+    {
+    }
+
+    #region Rough
+
+    public class SalaryCalculator
+    {
+        public float CalculateSalary(int hoursWorked, float hourlyRate) => hoursWorked * hourlyRate;
+    }
+
+    public class EmployeeDetails
+    {
+        public int HoursWorked { get; set; }
+        public int HourlyRate { get; set; }
+        public float GetSalary()
+        {
+            var salaryCalculator = new SalaryCalculator();
+            return salaryCalculator.CalculateSalary(HoursWorked, HourlyRate);
+        }
+    }
+    #endregion
+
+    #region Implementation
+
+    //public interface ISalaryCalculator
+    //{
+    //    float CalculateSalary(int hoursWorked, float hourlyRate);
+    //}
+
+    //public class SalaryCalculator : ISalaryCalculator
+    //{
+    //    public float CalculateSalary(int hoursWorked, float hourlyRate) => hoursWorked * hourlyRate;
+    //}
+
+    //public class EmployeeDetails
+    //{
+    //    private readonly ISalaryCalculator _salaryCalculator;
+    //    public int HoursWorked { get; set; }
+    //    public int HourlyRate { get; set; }
+    //    public EmployeeDetails(ISalaryCalculator salaryCalculator)
+    //    {
+    //        _salaryCalculator = salaryCalculator;
+    //    }
+    //    public float GetSalary()
+    //    {
+    //        return _salaryCalculator.CalculateSalary(HoursWorked, HourlyRate);
+    //    }
+    //}
+    #endregion
+}
